5c7e332d955a8e90590ad1f5ef4db4c6309b6199
Bhattarai-Manoj <bhattaraim299@gmail.com>
2024-09-08 00:01:56 +0545

    variable value and output
    I have changed the variable value from 28 to 2. And, I removed output line "Your age is".

--- Assignment for git-hub/src/App.java
+++ Assignment for git-hub/src/App.java
@@ -1,10 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int age= 28;
+        int age= 2;
         if(age>0 && age<20){
-            System.out.println("Your age is "+ age +". You are underage to take License.");
+            System.out.println(age +". You are underage to take License.");
          } else{
-            System.out.println("Your age is "+ age +". You are able to take a License.");
+            System.out.println(age +". You are able to take a License.");
          }
          System.out.println("Press space to exit");
     }
