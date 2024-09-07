879a073282d477bfa3fdfc8d5f593ee40a5248f4
Bhattarai-Manoj <bhattaraim299@gmail.com>
2024-09-07 23:48:10 +0545

    Initial commit

---
+++ Assignment for git-hub/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int age=12;
+        if(age>0 && age<18){
+            System.out.println("Your age is "+ age +". You are underage to take License.");
+         } else{
+            System.out.println("Your age is "+ age +". You are able to take a License.");
+         }
+         System.out.println("Press space to exit");
+    }
+}
