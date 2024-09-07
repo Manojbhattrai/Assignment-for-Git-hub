3e8f1b35dba8fd924df34d1b5eb72f68c3b914d9
Bhattarai-Manoj <bhattaraim299@gmail.com>
2024-09-07 23:57:10 +0545

    I have changed the value of condition from 18 to 20 and variable value to 28

--- Assignment for git-hub/src/App.java
+++ Assignment for git-hub/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int age= 56;
-        if(age>0 && age<18){
+        int age= 28;
+        if(age>0 && age<20){
             System.out.println("Your age is "+ age +". You are underage to take License.");
          } else{
             System.out.println("Your age is "+ age +". You are able to take a License.");
